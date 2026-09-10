a6cbe85af88cffafdf9a1e36d99e369033c5f2d2
initial2024 <zo46@qq.com>
2026-09-11 01:02:23 +0800

    Bump version to v0.13.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ccwu.orbitime"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.12.0"
+        versionCode = 13
+        versionName = "0.13.0"
     }
 
     compileOptions {
