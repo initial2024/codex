e45c306fb4f76f12a53432de9257ac3e9dfcde9d
initial2024 <zo46@qq.com>
2026-09-10 20:29:34 +0800

    Bump Orbit IME to v0.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ccwu.orbitime"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.10.0"
+        versionCode = 11
+        versionName = "0.11.0"
     }
 
     compileOptions {
